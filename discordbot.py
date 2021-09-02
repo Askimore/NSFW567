1b1510dd36e1e4473689b802abb7da6634970373
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-02 23:41:38 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,7 +24,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     #如果包含 ping，機器人回傳 pong
-    if message.content == '@'+client.user.name+'跳舞'
+    if message.content == '@'+'client.user.name'+'跳舞'
     await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')
     
     
