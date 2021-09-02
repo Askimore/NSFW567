4dd3bbe1ffed5cc6682ddd81a7ab385fe821568e
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-02 22:34:56 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -34,17 +34,17 @@ async def on_message(message):
             remessage = translator.translate(content, dest='zh-tw').text
             await message.reply(remessage) 
 #當有訊息時
-@client.event
 async def on_message(message):
     #排除自己的訊息，避免陷入無限循環
     if message.author == client.user:
         return
-    #如果包含 ping，機器人回傳 pong
-   if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+'跳舞')
-        await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')            
+    if message.author.bot:
+        return
+    if client.user in message.mentions: # @判定
+       robotName = client.user.name
+       message.content == 'ping':
+       first, space, content = message.clean_content.partition('@'+robotName+'跳舞')
+       await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')            
             
             
 # Bot起動
