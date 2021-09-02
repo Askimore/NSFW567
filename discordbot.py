31cd628f83c757251519419e1c64bece124e6f5e
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-02 23:03:08 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,33 +24,9 @@ async def on_message(message):
     if message.author.bot:
         return
     
-    if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+' ')
-        
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
-
-            
-@client.event
-#當有訊息時
-async def on_message(message):
-    #排除自己的訊息，避免陷入無限循環
-    if message.author == client.user:
-        return
-    if message.author.bot:
-        return
-    
      if client.user in message.mentions: # @判定
         robotName = client.user.name
-
-    if message.content == ('@'+robotName+' '):
+        message.content == ('@'+robotName+'跳舞'):
         await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')
 
             
