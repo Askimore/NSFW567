e50715d748100fac348d20ac9736a271be3c4e7e
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-02 23:31:35 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,15 +17,15 @@ async def on_ready():
     #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
     await client.change_presence(status=discord.Status.dnd, activity=game)
 
-# 收到訊息時呼叫
 @client.event
+#當有訊息時
 async def on_message(message):
-    # 送信者為Bot時無視
-    if message.author.bot:
+    #排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
         return
-    
-   if message.content == ('@'+robotname+'跳舞'):
-   await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')
+    #如果包含 ping，機器人回傳 pong
+    if message.content == ('@'+client.user.name+'跳舞')
+    await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')
     
     
 # Bot起動
