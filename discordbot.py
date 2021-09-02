6db277962882b7bd95baa21d7e80a1d3da91cbbc
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-02 23:48:03 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,10 +13,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('成功登入')
-    game = discord.Game('吹你的大雞巴')
+     game = discord.Game('吹你的大雞巴')
     #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
     await client.change_presence(status=discord.Status.dnd, activity=game)
 
+
+# 收到訊息時呼叫
 @client.event
 async def on_message(message):
     # 送信者為Bot時無視
@@ -26,8 +28,6 @@ async def on_message(message):
     #如果包含 ping，機器人回傳 pong
     if message.content == '跳舞'
     await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')
-    
-    
+
 # Bot起動
 client.run(TOKEN)
-
