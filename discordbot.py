7a703848442242c7fc0641f4bc8934930a4a9ea3
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-03 01:14:43 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,9 +13,14 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('成功登入')
-    game = discord.Game('你的小雞雞')
-    #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
-    await client.change_presence(status=discord.Status.dnd, activity=game)
+    #discord.Status.<狀態>，可以是online（上線）,offline（下線）,idle（閒置）,dnd（請勿打擾）,invisible（隱身）
+    status_w = discord.Status.dnd
+
+    #這邊設定機器當前的狀態文字
+    #type可以是playing（遊玩中）、streaming（直撥中）、listening（聆聽中）、watching（觀看中）、custom（自定義）
+    activity_w = discord.Activity(type=discord.ActivityType.streaming, name="吹妳老公的大雞巴", url="https://cdn.discordapp.com/attachments/856925480192311307/883036888305397810/ezgif-2-e44b0f4de4b1.gif")
+
+    await client.change_presence(status= status_w, activity=activity_w)
 
    
 @client.event
@@ -25,12 +30,10 @@ async def on_message(message):
         return
    
     if message.content == '跳個舞吧':
-        await asyncio.wait(3)
         await message.delete()
         
         tmpmsg = await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')
-        await asyncio.wait(9)
-        await tmpmsg.delete()
+                await tmpmsg.delete()
   
     
 # Bot起動
