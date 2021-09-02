8b7b2b4b439a4b82f2637ef18d68e54fdbd5131c
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-02 23:23:08 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,9 +23,11 @@ async def on_message(message):
     # 送信者為Bot時無視
     if message.author.bot:
         return
-    if robotName = client.user.name
-       message.content == ('@'+robotname+'跳舞'):
-        await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')
+    if client.user in message.mentions: # @判定
+       robotName = client.user.name
+       first, space, content = message.clean_content.partition('@'+robotName+'跳舞')
+    
+   await message.channel.send('https://cdn.discordapp.com/attachments/856925480192311307/882657302484770876/moiichan43_240835984_365616848349753_4194115607686417839_n.gif')
     
 
 # Bot起動
