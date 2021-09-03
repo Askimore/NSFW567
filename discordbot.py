03f01e663bee7330c586e95056ff3e598a4d9353
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-03 16:03:36 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,9 +11,15 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('成功登入')
+    #這邊設定機器人的狀態
+    #discord.Status.<狀態>，可以是online（上線）,offline（下線）,idle（閒置）,dnd（請勿打擾）,invisible（隱身）
     status_w = discord.Status.dnd
-    #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+
+    #這邊設定機器當前的狀態文字
+    #type可以是playing（遊玩中）、streaming（直撥中）、listening（聆聽中）、watching（觀看中）、custom（自定義）
     activity_w = discord.Activity(type=discord.ActivityType.streaming, name="吹喇叭", url="https://cn.pornhub.com/view_video.php?viewkey=ph60c597d48e037")
+    await client.change_presence(status= status_w, activity=activity_w)
+
 
 @client.event
 async def on_message(message):
