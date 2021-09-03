9819b7898c30efa75d111d0bbd516d2a7a43f77b
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-03 16:38:51 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,70 +26,97 @@ async def on_ready():
     activity_w = discord.Activity(type=discord.ActivityType.streaming, name="吹喇叭", url="https://cn.pornhub.com/view_video.php?viewkey=ph60c597d48e037")
     await client.change_presence(status= status_w, activity=activity_w)
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-  
-  nsfw_words = [“sexy”, “sexy”, “騷”, “賤”, “淫”]
-  starter_encouragements = [
-  "https://cdn.discordapp.com/attachments/869984495015714856/883262887999705118/dance.gif",
+import discord
+import os
+import requests
+import json
+import random
+from replit import db
+
+client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+
+starter_encouragements = [
   "https://cdn.discordapp.com/attachments/856925480192311307/881587108517666816/ezgif.com-gif-maker.gif",
+  "https://cdn.discordapp.com/attachments/869984495015714856/883262887999705118/dance.gif",
   "https://cdn.discordapp.com/attachments/869984495015714856/883267153749504000/ezgif.com-gif-maker.gif"
 ]
-  
-  async def on_message(message):
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+  return(quote)
+
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [encouraging_message]
+
+def delete_encouragment(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+  db["encouragements"] = encouragements
+
+@client.event
+async def on_ready():
+  print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
   if message.author == client.user:
     return
 
   msg = message.content
 
-  if msg.startswith('$inspire'):
+  if msg.startswith("$inspire"):
     quote = get_quote()
     await message.channel.send(quote)
+
+  if db["responding"]:
+    options = starter_encouragements
+    if "encouragements" in db.keys():
+      options = options + db["encouragements"]
+
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(options))
+
+  if msg.startswith("$new"):
+    encouraging_message = msg.split("$new ",1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added.")
+
+  if msg.startswith("$del"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      index = int(msg.split("$del",1)[1])
+      delete_encouragment(index)
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+  if msg.startswith("$list"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
     
-  if any(word in msg for word in nsfw_words):
-    await message.channel.send(random.choice(starter_encouragements))
-    
-    def update_encouragements(encouraging_message):
-  if "encouragements" in db.keys():
-    encouragements = db["encouragements"]
-    encouragements.append(encouraging_message)
-    db["encouragements"] = encouragements
-  else:
-    db["encouragements"] = [encouraging_message]
+  if msg.startswith("$responding"):
+    value = msg.split("$responding ",1)[1]
 
-def delete_encouragment(index):
-  encouragements = db["encouragements"]
-  if len(encouragements) > index:
-    del encouragements[index]
-  db["encouragements"] = encouragements
-  
-  def update_encouragements(encouraging_message):
-  if "encouragements" in db.keys():
-    encouragements = db["encouragements"]
-    encouragements.append(encouraging_message)
-    db["encouragements"] = encouragements
-  else:
-    db["encouragements"] = [encouraging_message]
-
-def delete_encouragment(index):
-  encouragements = db["encouragements"]
-  if len(encouragements) > index:
-    del encouragements[index]
-  db["encouragements"] = encouragements
-    
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('跳個舞吧'):
-        await message.channel.send('https://cdn.discordapp.com/attachments/869984495015714856/883262887999705118/dance.gif')  
-        await asyncio.sleep(9)
-        await message.delete()
-
+    if value.lower() == "true":
+      db["responding"] = True
+      await message.channel.send("Responding is on.")
+    else:
+      db["responding"] = False
+      await message.channel.send("Responding is off.")
 
     
 # Bot起動
