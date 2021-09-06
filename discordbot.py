180c0ac19928484f2aab2be1979c2732587baad8
Askimore <89986445+Askimore@users.noreply.github.com>
2021-09-07 01:17:29 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -257,7 +257,7 @@ jpg = [
 
 
 def get_photo():
-a = csv.reader(open(‘mp4.csv’,‘r’,encoding=‘UTF-8’))
+a = csv.reader(open(‘https://github.com/Askimore/NSFW567/blob/main/mp4.csv’,‘r’,encoding=‘UTF-8’))
 b = []
 for i in a:
 b.append(",".join(i))
